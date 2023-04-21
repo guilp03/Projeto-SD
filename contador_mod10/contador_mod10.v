cdf46f837961f6ad76341cbe0684fc7f4b844a42
guilp03 <108680176+guilp03@users.noreply.github.com>
2023-04-21 13:47:03 -0300

    a

--- contador_mod10/contador_mod10.v
+++ contador_mod10/contador_mod10.v
@@ -1,14 +1,15 @@
-module contador_mod10 (data, clk, load, enable, clear, count, count_end);
+module contador_mod10 (data, clk, load, enable, clear, count, count_end, tc);
   input wire [3:0] data;
   input wire clk, load, enable, clear;
   output reg [3:0] count;
   output wire count_end;
+  output wire tc;
 
   always @ (negedge clear)
   begin
     count <= 4'b0000;
   end
-
+  assign tc = enable & count_end;
   always @ (posedge clk)
   begin
     if (enable) begin
--- contador_mod_6/contador_mod_6.v
+++ contador_mod_6/contador_mod_6.v
@@ -1,29 +1,38 @@
-module contador_mod_6(data, loadn, clearn, clk, en, tens, tc, zero);
-    input wire [3:0] data;
-    input wire loadn, clearn, clk, en;
-    output reg [3:0] tens;
-    output wire tc, zero;
+module contador_mod_6(data, clk, loadn, en, clearn, tens, tc, zero);
+  input wire [3:0] data;
+  input wire clk, loadn, en, clearn;
+  output reg [3:0] tens;
+  output wire tc;
+  output wire zero;
 
-    assign data <= 4'b5;
-    always @(negedge clearn)begin
-        tens <= 4'b0;
-    end
-    assign zero <= (tens == 4'b0) ? 1'b1 : 1'b0;
-    assign tc = en && zero;
+  always @ (negedge clearn)
+  begin
+    tens = 4'b0000;
+  end
 
-    always @(posedge clk) begin
-        if (en) begin
-            case(tens)
-                4'b5: tens <= 4'b4; // 5 -> 4
-                4'b1: tens <= 4'b0; // 1 -> 0
-                4'b0: tens <= 4'b6; // 0 -> 6
-                default: tens <= tens - 4'b1;
-            endcase
-        end
-        else begin
-            if  (!load)begin
-                tens <= data;
-            end
-        end
+  assign zero = (tens == 4'b0000) ? 1'b1 : 1'b0;
+  assign tc = en & zero;
+
+  always @ (posedge clk)
+  begin
+    if (en) 
+      case (tens)
+        4'b0101: begin
+          tens <= 4'b0100;
+        end // 5 -> 4
+        4'b0001: begin 
+          tens <= 4'b0000;
+        end  // 1 -> 0
+        4'b0000: begin
+          tens <= 4'b0101;
+        end // 0 -> 5
+        default: tens <= tens - 4'b0001;
+      endcase
+
+    else begin
+
+      if(!loadn)
+        tens <= data;
     end
+  end
 endmodule
