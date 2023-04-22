dec72e64cda47988b553246fb461a8fa24d734e6
Vítor Gabriel <vggd@cin.ufpe.br>
2023-04-22 12:56:18 -0300

    Update entrada_timer.v

--- nivel1/nivel2/entrada_timer/entrada_timer.v
+++ nivel1/nivel2/entrada_timer/entrada_timer.v
@@ -9,5 +9,21 @@ module timer_controle(keypad, enablen, clk100, D, loadn, pgt_1Hz);
         output wire [3:0] D;
         output wire loadn, pgt_1Hz;
 
-        wire
+        wire clkf, Q;
+        reg keypad_on;
+        initial keypad_on = 0;
+
+        always@(keypad)begin
+                if(keypad == 10'b0000000000)
+                        keypad_on = 1'b0;            
+                else 
+                        keypad_on = 1'b1;
+        end
+
+        assign loadn = ~keypad_on
+
+        codificador_prioritario codificador_prioritario(keypad, enablen, D);
+        div_frequencia divisor(clk100,clkf);
+        contador_nao_reciclavel counter(clk100, loadn, Q);
+        mux mux(enablen, Q, clkf, pgt_1Hz);
 endmodule
