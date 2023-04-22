3d4c962f82dc228ad9c1bb89e3acd9557f26baea
Vítor Gabriel <vggd@cin.ufpe.br>
2023-04-22 11:17:55 -0300

    ajuste nas pastas e arquivos

---
+++ nivel1/nivel2/entrada_timer/entrada_timer.v
@@ -0,0 +1,6 @@
+module timer_controle(keypad, enablen, clk100, D, loadn, pgt_1Hz);
+        input wire [9:0] keypad;
+        input wire enablen clk100;
+        output wire [3:0] D;
+        output wire loadn, pgt_1Hz;
+endmodule
--- nivel2/timer_e_controle_nivel2/timer_controle.v
+++
@@ -1,6 +0,0 @@
-module timer_controle(keypad, clk, startn, stopn, clearn, door_closed, 
-                      blank_digit, sec_ones_segs, sec_tens_segs, min_segs);
-    
-    input wire [9:0] keypad;
-    input wire clk, startn, stopn, clearn, door_closed;
-    output wire [3:0] 
