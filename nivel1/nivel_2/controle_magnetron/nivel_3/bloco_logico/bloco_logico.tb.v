84a18624464838fbc0c624970c29dab0f989a715
guilp03 <108680176+guilp03@users.noreply.github.com>
2023-04-25 09:57:06 -0300

    testar o vcd

--- nivel1/nivel_2/controle_magnetron/nivel_3/bloco_logico/bloco_logico.tb.v
+++ nivel1/nivel_2/controle_magnetron/nivel_3/bloco_logico/bloco_logico.tb.v
@@ -24,6 +24,7 @@ module test_bloco_logico;
           	#5  startn=1; stopn=1; clearn=1; door_closed=1; timer_done=1;
             #5  startn=0; stopn=1; clearn=1; door_closed=1; timer_done=1;
             #5  startn=0; stopn=0; clearn=0; door_closed=0; timer_done=0;
+            #5  startn=0; stopn=1; clearn=1; door_closed=1; timer_done=0;
           
         end
 endmodule
